a17a4636090d2fabc7c041dd5bf2e2bb7b71a7ef
CCG226 <cown877@gmail.com>
2022-03-26 20:10:08 -0500

    Add project files.

---
+++ Word_puzzle/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+namespace Word_puzzle // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        enum characters { a,};
+        static void Main(string[] args)
+        {
+            var Randomizer = new Random();
+            char playAgain = 'y';
+            Console.WriteLine("Welcome to Random Word Search Puzzle Game");
+            while (playAgain == 'y')
+            {
+                var RandomWordPuzzleSize = Randomizer.Next(5, 21);
+
+                puzzle puzzle = new puzzle();
+                char[,] SearchPuzzle = puzzle.puzzle_Builder(RandomWordPuzzleSize,RandomWordPuzzleSize);
+            }
+     
+        }
+    }
+}
---
+++ Word_puzzle/puzzle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Word_puzzle
+{
+    internal class puzzle
+    {
+
+      public char[,] puzzle_Builder(int row, int col){ char[,] puzzle = new char[row, col]; return puzzle; }
+
+     //   void()
+    }
+}
