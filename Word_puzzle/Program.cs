6014357a43cf0da1eca56cee8bc7c7852c91577b
CCG226 <cown877@gmail.com>
2022-03-28 15:23:18 -0500

    StreamReader Fixed, works wit Dictionary file

--- Word_puzzle/Program.cs
+++ Word_puzzle/Program.cs
@@ -7,12 +7,17 @@ namespace Word_puzzle // Note: actual namespace depends on the project name.
      
         static void Main(string[] args)
         {
+            bool wordIsValid;
             int holdPuzzleCount = 0;
             var Randomizer = new Random();
             char playAgain = 'y';
+            int Score = 0;
+            string word = " ";
             Console.WriteLine("Welcome to Random Word Search Puzzle Game!");
-            Console.WriteLine("Play by finding words in the word search puzzle");
-           
+            Console.WriteLine();
+            Console.WriteLine("Goal: Enter as many valid words as you can find in each puzzzle");
+            Console.WriteLine("Each word is worth a 100 points");
+            
             
             while (playAgain == 'y')
             {
@@ -24,8 +29,13 @@ namespace Word_puzzle // Note: actual namespace depends on the project name.
                 New_puzzle.puzzleBuilder(RandomWordPuzzleSize, SearchPuzzle);
 
                 Console.WriteLine();
+                Console.WriteLine("Current score:" + Score);
                 Console.WriteLine("Puzzle: " + holdPuzzleCount);
                 New_puzzle.outputPuzzle(SearchPuzzle, RandomWordPuzzleSize);
+                Console.WriteLine();
+                Console.WriteLine("Enter a valid word");
+                word = Console.ReadLine();
+                wordIsValid = New_puzzle.DoesWordExist(word);
 
                 Console.WriteLine("Would you like to play again? Enter y to continue and n to exit.");
                 playAgain = Console.ReadLine()[0];
--- Word_puzzle/puzzle.cs
+++ Word_puzzle/puzzle.cs
@@ -6,41 +6,41 @@ using System.Threading.Tasks;
 
 namespace Word_puzzle
 {
-    public class puzzle
+    public partial class puzzle
     {
 
-      public char[,] Create_puzzle(int size){ char[,] wordPuzzle = new char[size, size]; return wordPuzzle; }
+        public char[,] Create_puzzle(int size) { char[,] wordPuzzle = new char[size, size]; return wordPuzzle; }
         public int Pull_Random_Value()
-        { 
+        {
             int RandomValue = new Random().Next(0, 1000000 + 1);
-           
+
             return RandomValue;
-            
+
         }
-     public void puzzleBuilder(int size, char[,] wordPuzzle)
-     {
-         
+        public void puzzleBuilder(int size, char[,] wordPuzzle)
+        {
+
             int GenerateValue = 0;
             letter_frequency getDict = new letter_frequency();
             letter_frequency[] LetterFrequencyChart = new letter_frequency[26];
-            LetterFrequencyChart =  getDict.Freq_Data();
-            for(int i = 0; i < size; i++)
+            LetterFrequencyChart = getDict.Freq_Data();
+            for (int i = 0; i < size; i++)
             {
-                for(int j = 0; j < size;j++)
+                for (int j = 0; j < size; j++)
                 {
                     GenerateValue = Pull_Random_Value();
                     wordPuzzle[i, j] = getDict.FreqMapper(LetterFrequencyChart, 0, GenerateValue);
-                  
+
                 }
             }
-            
-     }
-     public void outputPuzzle(char[,] wordPuzzle, int size)
+
+        }
+        public void outputPuzzle(char[,] wordPuzzle, int size)
         {
-         
-            for(int i = 0; i < size;i++)
+
+            for (int i = 0; i < size; i++)
             {
-                for(int j = 0; j < size;j++)
+                for (int j = 0; j < size; j++)
                 {
                     Console.Write(wordPuzzle[i, j] + " ");
                 }
@@ -48,6 +48,20 @@ namespace Word_puzzle
             }
         }
 
-   
+        public bool DoesWordExist(string word)
+        {
+          
+            using (StreamReader sr = new StreamReader(@"words.txt"))
+            {
+                string Data = sr.ReadToEnd();
+                if(Data.Contains(word))
+                {
+                    Console.WriteLine("No");
+                    return true;
+                }
+                return false;
+            }
+        }
     }
+    
 }
